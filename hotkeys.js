c920b224e30c1a8daeaca84b6111785f6cb42e55
pncl <anormalgecko@gmail.com>
2024-05-05 21:02:10 -0700

    miscellaenous cheat menu changes

--- hotkeys.js
+++ hotkeys.js
@@ -5,7 +5,7 @@ function keyPressed() {
   if (keyCode === 51) { game.mainPlayer.upgradeRegen(); }
   if (keyCode === 52) { game.mainPlayer.ability1.upgrade(game.mainPlayer); }
   if (keyCode === 53) { game.mainPlayer.ability2.upgrade(game.mainPlayer); }
-  if (keyCode === 70) { game.mainPlayer.speed = gameConsts.maxSpeed; game.mainPlayer.maxEnergy = gameConsts.maxEnergy; game.mainPlayer.regen = gameConsts.maxRegen; game.mainPlayer.upgradePoints = 99; }
+  if (keyCode === 70) { game.mainPlayer.speed = gameConsts.maxSpeed; game.mainPlayer.maxEnergy = gameConsts.maxEnergy; game.mainPlayer.regen = gameConsts.maxRegen; game.mainPlayer.upgradePoints = 160; for(var i = 0; i < 5; i++){game.mainPlayer.ability1.upgrade(game.mainPlayer)}; for(var i = 0; i < 5; i++){game.mainPlayer.ability2.upgrade(game.mainPlayer)};}
   if (keyCode === 27) { if (cheatMenuOpen){closeCheatMenu();} else {openCheatMenu();} }
   if (keyCode === 69) { game.mainPlayer.moveToAreaStart(); }
   if (keyCode === 84) { game.mainPlayer.moveToAreaEnd(); }
