6f2dd62f37abd7cb8d14e0905e613512928aca55
pncl <anormalgecko@gmail.com>
2024-05-19 15:48:15 -0700

    idk

--- heroes/rameses.js
+++ heroes/rameses.js
@@ -16,7 +16,7 @@ class Bandages extends Ability{
     this.applyingBandages = false;
   }
   activate(player, players, pellets, enemies, miscEnts, region, area){
-    if (this.bandagesApplied) this.cancelUse(player);
+    if (player.hasEffect("BandageEffect")) this.cancelUse(player);
     this.applyingBandages = true;
     player.gainEffect(new BandagingEffect(this.currentCooldown, this.slowdown));
   }
@@ -27,6 +27,13 @@ class Bandages extends Ability{
   }
   giveBandages(player){
     player.gainEffect(new BandageEffect(this.unbandagingDuration));
+    //remove bandaging effect if recharged early early
+    for (let i = 0; i < player.effects.length; i++){
+      if (player.effects[i].constructor.name === "BandagingEffect"){
+        player.effects.splice(i, 1);
+        i--;
+      }
+    }
     this.applyingBandages = false;
   }
 }
