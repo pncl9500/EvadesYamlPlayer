a374f9f99d5651fc81c63b458ab15d92cac9bb97
pncl <anormalgecko@gmail.com>
2024-07-09 15:25:49 -0700

    sugar rush

--- heroes/candy.js
+++ heroes/candy.js
@@ -13,9 +13,28 @@ class SugarRush extends Ability{
     this.duration = 2000;
     this.effectDuration = 2000;
     this.slow = 0.05; //???
+    this.aura = new LockedAura({x: 0, y: 0}, 120, "#ff80bd40", z.genericAura + random() * z.randEpsilon);
+    this.auraLife = 0;
+    this.radius = 400;
+    this.baseRadius = 100;
+    this.radiusPerSpeed = 5;
   }
   activate(player, players, pellets, enemies, miscEnts, region, area){
-    
+    this.aura.parent = player;
+    this.auraLife = 2000;
+    this.aura.toRemove = false;
+    player.auras.push(this.aura);
+  }
+  behavior(player, players, pellets, enemies, miscEnts, region, area){
+    this.auraLife -= dTime;
+    //let moveMag = Math.sqrt(player.ctrlVector.x ** 2 + player.ctrlVector.y ** 2);
+    let moveMag = Math.max(Math.abs(player.ctrlVector.x), Math.abs(player.ctrlVector.y))
+    let addedRadius = player.tempSpeed * moveMag * this.radiusPerSpeed;
+    this.radius = this.baseRadius + addedRadius;
+    this.aura.radius = this.radius;
+    if (this.auraLife < 0){
+      this.aura.toRemove = true;
+    }
   }
 }
 
@@ -40,6 +59,7 @@ class SweetToothItem extends Projectile{
     this.regenBoost = regenBoost;
     this.speedBoost = speedBoost;
     this.energyBoost = energyBoost;
+    this.renderOnMinimap = false;
   }
   detectContact(){
     this.detectPlayerContact();
